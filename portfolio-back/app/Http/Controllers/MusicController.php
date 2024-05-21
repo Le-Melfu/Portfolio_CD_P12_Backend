afa16d3ae27841d7214b9bf322be2022beba28ed
Melfu <degardenzi.clement@gmail.com>
2024-05-21 12:43:39 +0200

    audio working but search slider issue now

--- portfolio-back/app/Http/Controllers/MusicController.php
+++ portfolio-back/app/Http/Controllers/MusicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class MusicController extends Controller
 {
@@ -12,32 +13,32 @@ class MusicController extends Controller
             [
                 'id' => 1,
                 'title' => 'Au Jazz de Lune - Clément Degardenzi (composition court-métrage)',
-                'audioSrc' => asset('storage/media/music/Au Jazz de Lune (court métrage).mp3'),
+                'audioSrc' => Storage::url('media/music/Au_Jazz_de_Lune_(court_metrage).mp3')
             ],
             [
                 'id' => 2,
                 'title' => 'Waves - Guthrie Govan (cover guitare)',
-                'audioSrc' => asset('storage/media/music/Waves.mp3'),
+                'audioSrc' => Storage::url('media/music/Waves.mp3'),
             ],
             [
                 'id' => 3,
                 'title' => 'Locked out of heaven - Bruno Mars (cover basse)',
-                'audioSrc' => asset('storage/media/music/locked out of heaven.mp3'),
+                'audioSrc' => Storage::url('media/music/locked_out_of_heaven.mp3'),
             ],
             [
                 'id' => 4,
                 'title' => 'Summon the Megalodon - Sea of Thieves (cover MAO)',
-                'audioSrc' => asset('storage/media/music/Summon the Megalodon.mp3'),
+                'audioSrc' => Storage::url('media/music/Summon_the_Megalodon.mp3'),
             ],
             [
                 'id' => 5,
                 'title' => 'Nemo - Nightwish (cover guitare)',
-                'audioSrc' => asset('storage/media/music/nemo.mp3'),
+                'audioSrc' => Storage::url('media/music/nemo.mp3'),
             ],
             [
                 'id' => 6,
                 'title' => 'Silvera - Gojira (cover guitare)',
-                'audioSrc' => asset('storage/media/music/gojira - silvera.mp3'),
+                'audioSrc' => Storage::url('media/music/gojira_silvera.mp3'),
             ],
         ];
 
--- portfolio-back/app/Http/Controllers/ProjectController.php
+++ portfolio-back/app/Http/Controllers/ProjectController.php
@@ -15,7 +15,7 @@ class ProjectController extends Controller
                 'desc' => ['Ce projet était une initiation aux bases du HTML et du CSS'],
                 'image' => asset('storage/media/img/riding-cities.webp'),
                 'imageAlt' => 'Visuel du site',
-                'pin' => 'path/to/Logo_OpenClassrooms.png',
+                'pin' => asset('storage/media/Logo_OpenClassrooms.png'),
                 'pinAlt' => 'Logo OpenClassRooms',
             ],
             [
@@ -25,7 +25,7 @@ class ProjectController extends Controller
                     'Ce projet consistait en l\'élaboration de la page d\'accueil d\'une agence de location immobilère en utilisant HTML et CSS avec les grid et les flexbox',
                 ],
                 'url' => 'https://github.com/Le-Melfu/booki-starter-code',
-                'pin' => 'path/to/Logo_OpenClassrooms.png',
+                'pin' => asset('storage/media/Logo_OpenClassrooms.png'),
                 'pinAlt' => 'Logo OpenClassRooms',
             ],
             [
@@ -35,7 +35,7 @@ class ProjectController extends Controller
                     'Ce projet consistait en l\'élaboration d\'animations simple afin de dynamiser la page d\'une application de restauration',
                 ],
                 'url' => 'https://github.com/Le-Melfu/Site_OhMyFood',
-                'pin' => 'path/to/Logo_OpenClassrooms.png',
+                'pin' => asset('storage/media/Logo_OpenClassrooms.png'),
                 'pinAlt' => 'Logo OpenClassRooms',
             ],
             [
@@ -45,7 +45,7 @@ class ProjectController extends Controller
                     'Ce projet m\'a permis d\'appréhender les bases de JAVASCRIPT dans la réalisation d\'un carousel dynamique',
                 ],
                 'url' => 'https://github.com/Le-Melfu/Print-it-JS',
-                'pin' => 'path/to/Logo_OpenClassrooms.png',
+                'pin' => asset('storage/media/Logo_OpenClassrooms.png'),
                 'pinAlt' => 'Logo OpenClassRooms',
             ],
             [
@@ -55,7 +55,7 @@ class ProjectController extends Controller
                     'Dans ce projet j\'ai pu apprendre le fonctionnement des API et comment s\'en servir au sein d\'une application classique HTML/CSS/JS grâce aux différentes requêtes',
                 ],
                 'url' => 'https://github.com/Le-Melfu/Portfolio-architecte-sophie-bluel',
-                'pin' => 'path/to/Logo_OpenClassrooms.png',
+                'pin' => asset('storage/media/Logo_OpenClassrooms.png'),
                 'pinAlt' => 'Logo OpenClassRooms',
             ],
             [
@@ -65,7 +65,7 @@ class ProjectController extends Controller
                     'Ce projet m\'a permis d\'apprendre les bases de fonctionnement de React pour créer une Single Page Application',
                 ],
                 'url' => 'https://github.com/Le-Melfu/Kasa',
-                'pin' => 'path/to/Logo_OpenClassrooms.png',
+                'pin' => asset('storage/media/Logo_OpenClassrooms.png'),
                 'pinAlt' => 'Logo OpenClassRooms',
             ],
             [
@@ -75,7 +75,7 @@ class ProjectController extends Controller
                     'Dans ce projet j\'ai du optimiser le site internet d\'une photographe afin que celui-ci gagne en performance, en accessibilité et en SEO.',
                 ],
                 'url' => 'https://github.com/Le-Melfu/ninacarducci.github.io',
-                'pin' => 'path/to/Logo_OpenClassrooms.png',
+                'pin' => asset('storage/media/Logo_OpenClassrooms.png'),
                 'pinAlt' => 'Logo OpenClassRooms',
             ],
             [
@@ -86,7 +86,7 @@ class ProjectController extends Controller
                     'J\'ai également dù écrire plusieurs tests unitaire et aborder cet aspect du travail de développeur WEB',
                 ],
                 'url' => 'https://github.com/Le-Melfu/Debuggez-une-application-React.JS',
-                'pin' => 'path/to/Logo_OpenClassrooms.png',
+                'pin' => asset('storage/media/Logo_OpenClassrooms.png'),
                 'pinAlt' => 'Logo OpenClassRooms',
             ],
         ];
